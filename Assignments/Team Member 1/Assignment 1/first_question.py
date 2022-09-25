5eb82f22d47308232085d18978dd0a650fcea9e5
Mohammed Raushan <113915370+MohammedRaushan@users.noreply.github.com>
2022-09-25 10:21:10 +0530

    Update first_question.py

--- Assignments/Team Member 1/Assignment 1/first_question.py
+++ Assignments/Team Member 1/Assignment 1/first_question.py
@@ -1,1 +1,17 @@
-
+from flask import Flask, request, render_template
+ 
+app = Flask(__name__)  
+ 
+@app.route('/', methods =["GET", "POST"])
+def get_info():
+    if request.method == "POST":
+       name = request.form.get("name")
+       email = request.form.get("email")
+       phone = request.form.get("ph_no")
+       values = f"<h1>Name : {name}</h1><h1>Email : {email}</h1><h1>Phone Number : {phone}</h1>"
+       return values
+       
+    return render_template("login.html")
+ 
+if __name__=='__main__':
+   app.run(debug=True)
