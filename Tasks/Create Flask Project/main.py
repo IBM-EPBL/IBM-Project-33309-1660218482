80eabebdbb3806c515698013dc3c949128bf4a4b
Mohammed Raushan <113915370+MohammedRaushan@users.noreply.github.com>
2022-10-13 19:28:59 +0530

    Update main.py

--- Tasks/Create Flask Project/main.py
+++ Tasks/Create Flask Project/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 #Creating Home Page
 @app.route("/")
 def home():
-  return render_template("index.html")
+  return "Smart Fashion Recommender System"
 
 if __name__ == "__main__":
   app.run(debug=True)
