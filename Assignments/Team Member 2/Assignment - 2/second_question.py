c1f45742a9bd372630e3b4ba92a216117230d003
Aswath <113688805+Aswath23012002@users.noreply.github.com>
2022-11-09 18:59:14 +0530

    Modified

--- Assignments/Team Member 2/Assignment - 2/second_question.py
+++ Assignments/Team Member 2/Assignment - 2/second_question.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, make_response
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response
 
 app = Flask(__name__)
 @app.route('/cookie/')
